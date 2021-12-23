29a998591c4c5949909c1d8d10f2cd157f754d01
Wanda Wang <chuwang@wustl.edu>
2021-12-23 12:48:46 -0800

    Completed disc05 - took too long! need more sleep..

---
+++ disc/disc08.py
@@ -0,0 +1,195 @@
+class A:
+    def __init__(self, x):
+        self.x = x
+
+    def __repr__(self):
+         return self.x
+
+    def __str__(self):
+         return self.x * 2
+
+class B:
+    def __init__(self):
+         print('boo!')
+         self.a = []
+
+    def add_a(self, a):
+         self.a.append(a)
+
+    def __repr__(self):
+         print(len(self.a))
+         ret = ''
+         for a in self.a:
+             ret += str(a)
+         return ret
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+ganondorf = Link('zelda', Link('link', Link('sheik', Link.empty)))
+print(ganondorf.rest.rest.first)
+print(ganondorf.rest.first)
+
+
+def sum_nums(s):
+    """
+    >>> a = Link(1, Link(6, Link(7)))
+    >>> sum_nums(a)
+    14
+    """
+    "*** YOUR CODE HERE ***"
+    if s.rest is Link.empty:
+        return s.first
+    return s.first + sum_nums(s.rest)
+
+def multiply_lnks(lst_of_lnks):
+    """
+    >>> a = Link(2, Link(3, Link(5)))
+    >>> b = Link(6, Link(4, Link(2)))
+    >>> c = Link(4, Link(1, Link(0, Link(2))))
+    >>> p = multiply_lnks([a, b, c])
+    >>> p.first
+    48
+    >>> p.rest.first
+    12
+    >>> p.rest.rest.rest is Link.empty
+    True
+    """
+    res = Link(0)  # Dummy head
+    p = res
+    while True:
+        number = 1
+        index = 0
+        while index < len(lst_of_lnks):
+            lst = lst_of_lnks[index]
+            if lst is Link.empty:
+                return res.rest
+            number *= lst.first
+            lst_of_lnks[index] = lst.rest
+            index += 1
+        p.rest = Link(number)
+        p = p.rest
+    return res.rest
+
+
+def flip_two(s):
+    """
+    >>> one_lnk = Link(1)
+    >>> flip_two(one_lnk)
+    >>> one_lnk
+    Link(1)
+    >>> lnk = Link(1, Link(2, Link(3, Link(4, Link(5)))))
+    >>> flip_two(lnk)
+    >>> lnk
+    Link(2, Link(1, Link(4, Link(3, Link(5)))))
+    """
+    "*** YOUR CODE HERE ***"
+
+    # For an extra challenge, try writing out an iterative approach as well below!
+    "*** YOUR CODE HERE ***"
+    # Per doctest, we can't return anything. Hence we cannot change the rest pointer
+    # and can only change the value.
+    if s is Link.empty or s.rest is Link.empty:
+        return  # do nothing
+    flip_two(s.rest.rest)
+    temp = s.rest.first
+    s.rest.first = s.first
+    s.first = temp
+    
+
+class Tree:
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = branches
+
+    def is_leaf(self):
+        return not self.branches
+
+def make_even(t):
+    """
+    >>> t = Tree(1, [Tree(2, [Tree(3)]), Tree(4), Tree(5)])
+    >>> make_even(t)
+    >>> t.label
+    2
+    >>> t.branches[0].branches[0].label
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    t.label += t.label % 2
+    for b in t.branches:
+        make_even(b)
+
+def leaves(t):
+    """Returns a list of all the labels of the leaf nodes of the Tree t.
+
+    >>> leaves(Tree(1))
+    [1]
+    >>> leaves(Tree(1, [Tree(2, [Tree(3)]), Tree(4)]))
+    [3, 4]
+    """
+    "*** YOUR CODE HERE ***"
+    if t.is_leaf():
+        return [t.label]
+    res = []
+    for b in t.branches:
+        res.extend(sum([leaves(b)], [])) # must make leaves(b) to [] since sum will take out the element to concatenate list
+    return res
+
+
+def find_paths(t, entry):
+    """
+    >>> tree_ex = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(1, [Tree(5)])])
+    >>> find_paths(tree_ex, 5)
+    [[2, 7, 6, 5], [2, 1, 5]]
+    >>> find_paths(tree_ex, 12)
+    []
+    """
+
+    paths = []
+    if t.label == entry:
+        paths.append([t.label])
+    for b in t.branches:
+        for path in find_paths(b, entry):
+            paths.append([t.label] + path)
+    return paths
